17be9aa5cafc4d5f9b80094ff3f732d6845a0d09
Balakumaran <bgovindaraj@paypal.com>
2021-07-08 13:40:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css';
+
+import React, { Suspense } from 'react';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch
+} from "react-router-dom";
+
+const Details = React.lazy(() => import('./Details'));
+const Home = React.lazy(() => import('./Home'));
+const Navbar = React.lazy(() => import('./Navbar'));
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+       <Suspense fallback={<div>Loading...</div>}>
+        <Navbar />
+
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
